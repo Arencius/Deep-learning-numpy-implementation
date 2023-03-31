8817d8442af93ccd5d4f3d05e1e2707b5224cc07
karol <karol.krawczyk1@edu.uekat.pl>
2023-03-31 19:05:15 +0200

    Added Layer class as a template for parameterized layer classes

--- src/layers/layer.py
+++ src/layers/layer.py
@@ -2,13 +2,38 @@ from abc import ABC, abstractmethod
 
 
 class BaseLayer(ABC):
-    def __init__(self, *args, **kwargs):
-        pass
-
     @abstractmethod
     def forward(self, *args):
         raise NotImplementedError
 
     @abstractmethod
     def backward(self, *args):
+        raise NotImplementedError
+
+
+class Layer(BaseLayer):
+    def __repr__(self):
+        return f'''
+        ----------------------------------
+        Name: {self.__class__.__name__}
+        Parameters: {self.parameters}
+        Output shape: {self.output_shape}
+        ----------------------------------
+        '''
+
+    @property
+    def parameters(self):
+        return 0
+
+    @property
+    def output_shape(self):
+        return 0
+
+    def _initialize_weights(self):
+        raise NotImplementedError
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def backward(self, output_gradients):
         raise NotImplementedError
